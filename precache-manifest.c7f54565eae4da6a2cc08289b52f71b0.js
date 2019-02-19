74d37c1f1b36fb5cfcc8c772214635211908a970
davidzki <d@videriksson.com>
2019-02-19 12:35:49 +0100

    Updates

---
+++ precache-manifest.c7f54565eae4da6a2cc08289b52f71b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e889fed1dd8168e068ac",
+    "url": "/davidzki/design-challenge.git/static/js/runtime~main.e889fed1.js"
+  },
+  {
+    "revision": "1a83f2adad8ae35d18cc",
+    "url": "/davidzki/design-challenge.git/static/js/main.1a83f2ad.chunk.js"
+  },
+  {
+    "revision": "9bd6be4b1afa8fe7f1b2",
+    "url": "/davidzki/design-challenge.git/static/js/2.9bd6be4b.chunk.js"
+  },
+  {
+    "revision": "1a83f2adad8ae35d18cc",
+    "url": "/davidzki/design-challenge.git/static/css/main.684e9245.chunk.css"
+  },
+  {
+    "revision": "70dec812e18f967250874fc2de0c3875",
+    "url": "/davidzki/design-challenge.git/index.html"
+  }
+];
